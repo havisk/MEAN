63728de2632b9b30f6e89eac9a87a157fd3c5dcc
Kool Havis <havisk@gmail.com>
2016-02-27 19:04:47 -0500

    can get user by id

--- server.js
+++ server.js
@@ -131,7 +131,16 @@ apiRouter.route('/users')
 apiRouter.route('/users/:user_id')
 
 //get the user with that id (accessed at GET http://localhost:8080/api/users/:user_id)
+.get(function(req, res){
 
+  User.findById(req.params.user_id, function(err, user){
+    if(err)
+      res.send(err);
+
+    //return that user
+    res.json(user);
+  });
+})
 
 
 // REGISTER OUR ROUTES ------------------------------------------------
